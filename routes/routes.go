118eb4000f8bfd006d0bb5c5f93901133b5b34e6
dani_edo <dani.edogawa.17@gmail.com>
2025-03-18 13:40:08 +0700

    use route group

--- routes/routes.go
+++ routes/routes.go
@@ -7,10 +7,13 @@ import (
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
-	server.POST("/events", middlewares.Authenticate, createEvent)
 	server.GET("/events/:id", getEvent)
-	server.PUT("/events/:id", middlewares.Authenticate, updateEvent)
-	server.DELETE("/events/:id", middlewares.Authenticate, deleteEvent)
+
+	autenticated := server.Group("/")
+	autenticated.Use(middlewares.Authenticate)
+	autenticated.POST("/events", middlewares.Authenticate, createEvent)
+	autenticated.PUT("/events/:id", middlewares.Authenticate, updateEvent)
+	autenticated.DELETE("/events/:id", middlewares.Authenticate, deleteEvent)
 
 	server.POST("/signup", signUp)
 	server.POST("/login", login)
