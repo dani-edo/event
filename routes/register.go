7a5e76de3c94062e5770aa96556caf7b5a930e8f
dani_edo <dani.edogawa.17@gmail.com>
2025-03-21 20:56:04 +0700

    cancel registration event

--- routes/register.go
+++ routes/register.go
@@ -31,4 +31,21 @@ func registerForEvent(context *gin.Context) {
 	context.JSON(http.StatusCreated, gin.H{"message": "Registered for event", "event": event})
 }
 
-func cancelRegistration(context *gin.Context) {}
+func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID", "error": err.Error()})
+		return
+	}
+
+	var event models.Event
+	event.ID = id
+
+	err = event.CancelRegistration(userId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not cancel registration", "error": err.Error()})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{"message": "Registration cancelled", "event": event})
+}
